7df355abfef24ad8f5da5fde8d585236380c9b59
NexonSU <root@nexon.su>
2021-09-04 15:58:55 +0300

    Some improvements

--- bot.py
+++ bot.py
@@ -1,10 +1,11 @@
+from PIL.ImageOps import grayscale
 import pyautogui
 import time
 import threading
 import os
 from pynput import keyboard
 
-pyautogui.PAUSE = 0.5
+pyautogui.PAUSE = 0.01
 
 ClickerEnabled = True
 GoldClickerEnabled = True
@@ -15,11 +16,11 @@ def SearchAndClick(file, confidenceValue):
         CW = pyautogui.getActiveWindow()
         if CW is not None and "Cookie Clicker" in CW.title and GoldClickerEnabled:
             try:
-                pos = pyautogui.locateCenterOnScreen(file, confidence=confidenceValue)
-                pyautogui.click(pos.x, pos.y)
+                pos = pyautogui.locateCenterOnScreen(file, grayscale=True, confidence=confidenceValue)
+                pyautogui.moveTo(pos.x, pos.y)
             except:
                 pass
-            time.sleep(0.25)
+            time.sleep(5)
 
 def AutoBuy():
     while True:
@@ -43,7 +44,7 @@ def ClickTheCookie():
     while True:
         CW = pyautogui.getActiveWindow()
         if CW is not None and "Cookie Clicker" in CW.title and ClickerEnabled:
-            pyautogui.click(CW.width*0.15+CW.left, CW.height*0.39+CW.top, clicks=10, interval=0.001)
+            pyautogui.click(CW.width*0.15+CW.left, CW.height*0.39+CW.top, clicks=10, interval=0.001, _pause=False)
 
 def ToogleClicker():
     global ClickerEnabled
@@ -69,17 +70,22 @@ def ToogleBuyer():
 def Exit():
     os._exit(0)
 
-searcher = threading.Thread(target=SearchAndClick, args=('goldCookie3.png', 0.4))
+searcher = threading.Thread(target=SearchAndClick, args=('goldCookie.png', .5))
+userSearcher = threading.Thread(target=SearchAndClick, args=('goldCookie_user.png', .5))
 clicker = threading.Thread(target=ClickTheCookie)
 buyer = threading.Thread(target=AutoBuy)
 
 searcher.start()
+userSearcher.start()
 clicker.start()
 buyer.start()
 
 with keyboard.GlobalHotKeys({
         '<esc>': Exit,
         '<ctrl>+<alt>+c': ToogleClicker,
+        '<ctrl>+<alt>+с': ToogleClicker,
         '<ctrl>+<alt>+g': ToogleGoldClicker,
-        '<ctrl>+<alt>+b': ToogleBuyer}) as h:
+        '<ctrl>+<alt>+п': ToogleGoldClicker,
+        '<ctrl>+<alt>+b': ToogleBuyer,
+        '<ctrl>+<alt>+и': ToogleBuyer}) as h:
     h.join()
